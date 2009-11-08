bf5014ce4b5be326f8883050e278eea0d21f37b2
Mat Holroyd <matholroyd@gmail.com>
2009-11-08 12:26:40 +1100

    Fixed the activity responder for create

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,4 +5,13 @@ class ActivitiesController < ApplicationController
     actions :all
     publish :json, :attributes => %w{id title}
   end
+  
+  def create
+    request.body.each{|body_part|
+      params = ActiveSupport::JSON.decode(body_part)
+      logger.debug(params.inspect)
+      Activity.create! params['activity']
+    }
+    render :nothing => true
+  end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,3 @@
 class Activity < ActiveRecord::Base
+  validates_presence_of :title
 end
