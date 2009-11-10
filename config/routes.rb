98c93461ff94de064147fcfdfcfd2e913c5a0a20
Mat Holroyd <matholroyd@gmail.com>
2009-11-10 17:24:41 +1100

    Got map page mostly setup to start trying to do the twitter bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource :user_session, :except => [:edit, :show, :update]
-  map.resources :tworgies
+  map.resources :tworgies, :collection => {:twitter_callback => :get}
   map.resources :users
   map.resources :posts
   map.resources :activities
