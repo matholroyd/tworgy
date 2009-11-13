6e97478c7489936d22e983fe1987a5af4686cf6b
Mat Holroyd <matholroyd@gmail.com>
2009-11-13 17:13:32 +1100

    Trying to get hookups to work on engineyard...

--- app/controllers/tworgies_controller.rb
+++ app/controllers/tworgies_controller.rb
@@ -11,7 +11,7 @@ class TworgiesController < ApplicationController
     request_token = @client.request_token
     session[:request_token] = request_token.token
     session[:request_token_secret] = request_token.secret
-    redirect_to request_token.authorize_url
+    redirect_to request_token.authorize_url.gsub('authorize', 'authenticate') 
   end
   
   def twitter_callback
